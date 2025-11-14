7cb0c8341ec7f0927447be636ad674dd890d797c
ChiaraBm <chiara.bm04@gmail.com>
2025-11-14 08:59:53 +0100

    Removed old checkbox. Added new checkbox component

--- ShadcnBlazor.Test/Program.cs
+++ ShadcnBlazor.Test/Program.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ShadcnBlazor.Interop;
 using ShadcnBlazor.Test.UI;
-using ShadcnBlazor.Test.UI.Components;
 using TailwindMerge.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
