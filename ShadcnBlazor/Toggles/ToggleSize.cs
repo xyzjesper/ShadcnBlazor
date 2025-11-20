e6c7ab8982355ac281f91d68c5a26696e93a1ce4
ChiaraBm <chiara.bm04@gmail.com>
2025-11-20 22:10:59 +0100

    Added toggle component

---
+++ ShadcnBlazor/Toggles/ToggleSize.cs
@@ -0,0 +1,8 @@
+namespace ShadcnBlazor.Toggles;
+
+public enum ToggleSize
+{
+    Default = 0,
+    Sm = 1,
+    Lg = 2
+}
---
+++ ShadcnBlazor/Toggles/ToggleVariant.cs
@@ -0,0 +1,7 @@
+namespace ShadcnBlazor.Toggles;
+
+public enum ToggleVariant
+{
+    Default = 0,
+    Outline = 1
+}
